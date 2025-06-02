a3a2a5f9e34545d973c593f3cc5f21c5bf76e0c3
Julio Amaru Cerda Villablanca <julio.cerdav@sansano.usm.cl>
2025-06-02 13:30:32 -0400

    Se actualiza el nombre de los certificados cvss y cwe

--- backend/src/routes/cwe.js
+++ backend/src/routes/cwe.js
@@ -35,7 +35,7 @@ module.exports = function (app) {
           'Content-Type': 'application/json',
           'Content-Length': Buffer.byteLength(vuln),
         },
-        ca: fs.readFileSync(__dirname + '/../../ssl/cwe_api.cert'),
+        ca: fs.readFileSync(__dirname + '/../../ssl/cwe_api.crt'),
         rejectUnauthorized: true,
         timeout: TIMEOUT_MS,
       };
