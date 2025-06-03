52fb8807ebcb271327e72bf7697e48360937fe7b
Julio Amaru Cerda Villablanca <julio.cerdav@sansano.usm.cl>
2025-06-03 16:11:52 -0400

    fix: path to certificate

--- backend/src/lib/cwe-api-cert.js
+++ backend/src/lib/cwe-api-cert.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const CWE_API_CERT = fs.readFileSync(
-  path.join(__dirname, '../ssl/cwe_api.crt'),
+  path.join(__dirname, '../../ssl/cwe_api.crt'),
 );
 
 module.exports = CWE_API_CERT;
